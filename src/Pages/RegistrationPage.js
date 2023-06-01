e9a0a65c09809f54461285922442cabff56e2fb5
VladimirTheRPQer <vsk140803@gmail.com>
2023-06-01 16:40:24 +0300

    modified:   public/index.html 	modified:   src/Pages/EnterPage.js 	modified:   src/components/Header.js 	modified:   src/css/Header.css 	modified:   src/index.js

--- src/Pages/RegistrationPage.js
+++ src/Pages/RegistrationPage.js
@@ -7,7 +7,22 @@ class RegistrationPage extends React.Component{
         return(
             <div>
                 <Header/>
-                <h1>Регистрация</h1>
+                <div className='login'>
+                    <div className='login-form'>
+                        <form>
+                            <br></br>
+                            <h3>Имя пользователя:</h3>
+                            <input name = 'userName' type = "text" placeholder='Введите имя пользователя'/><br></br>
+                            <h3>Email:</h3>
+                            <input name = 'email' type = "text" placeholder='Введите email'/><br></br>
+                            <h3>Пароль:</h3>
+                            <input name = 'password' type = "password" placeholder='Введите пароль'/><br></br>
+                            <input name = 'password' type = "password" placeholder='Подтвердите пароль'/><br></br>
+                            <button type = 'submit'>Зарегистрироваться</button>
+                        </form>
+                    </div>
+                </div>
+                    
             </div>
         )
     }
